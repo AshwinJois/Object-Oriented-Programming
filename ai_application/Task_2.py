8a6ac3a9514603f94b9f64b7885a0a4b62435e86
Ashwin M Jois <63425115+AshwinJois@users.noreply.github.com>
2021-04-22 10:41:57 +0530

    Add files via upload

---
+++ ai_application/Task_2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 14:53:29 2021
+
+@author: ashwi
+"""
+
+import tensorflow as tf
+import sys
+sys.path.append('G:/ICS_Sylabus/Object_Oriented_Programming/repo_ai_application')
+from __try___ import Dataset_Generation
+from __try___ import Network_layers
+
+mnist = tf.keras.datasets.mnist # 28x28 images of handwritten digits 0-9
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+dataset_mnist = Dataset_Generation(x_train,y_train, x_test, y_test,10)
+
+Network_mnist = Network_layers(x_train,y_train, x_test, y_test,10)
+
+Network_mnist.layers()
+Network_mnist.compile_fit_model()
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+model = tf.keras.models.load_model('C:/Users/ashwi/OneDrive/Desktop/Travel March 2021/models', 
+                                          compile=True)
+
+prediction = model.predict([x_test])
+print(prediction)
+
+print(np.argmax(prediction[1000]))
+plt.imshow(x_test[1000])
+"""
---
+++ ai_application/ai_library/Task_1.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 12:38:26 2021
+
+@author: ashwi
+"""
+import tensorflow as tf
+import datetime, os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pickle
+
+class Dataset_Generation:
+    
+    def __init__(self,x,y,a,b,output):
+        self.x = x # train_dataset
+        self.y = y # validation_dataset
+        self.a = a # Used for MNIST
+        self.b = b # Used for MNIST 
+        self.output = output # Output classes of custom generated data is 3 = Circle, Rectangle, Triangle
+                             # Output classes of MNIST is 10 = 0 to 9    
+     
+class Network_layers(Dataset_Generation):  
+    
+    def layers(self):        
+        self.model = tf.keras.models.Sequential()
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) 
+        self.model.add(tf.keras.layers.Dense(self.output, activation=tf.nn.softmax))  
+        
+    def compile_fit_model(self):
+        self.model.compile(loss = 'sparse_categorical_crossentropy',
+                      optimizer = 'adam',
+                      metrics = ['accuracy']) 
+        
+        logdir = os.path.join("G:/ICS_Sylabus/Object_Oriented_Programming/tensorboard", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)              
+        
+        callbacks = [tensorboard_callback,
+            tf.keras.callbacks.ModelCheckpoint(
+                filepath='C:/Users/ashwi/OneDrive/Desktop/Travel March 2021/models',
+                save_best_only=True,  # Saves model if `loss` has improved.
+                monitor="loss",
+                verbose=1,
+            )
+        ]
+               
+        try:      # For Custom Generated Data  
+            hist = self.model.fit(self.x, validation_data = self.y, epochs=29, callbacks=callbacks, shuffle=True) 
+        
+        except:   # For MNIST dataset
+            hist = self.model.fit(self.x, self.y, validation_data = (self.a, self.b),epochs = 30, callbacks=callbacks, shuffle=True)
+        
+        hist.history        
+
+        f = open('C:/Users/ashwi/OneDrive/Desktop/Travel March 2021/history.pckl', 'wb')
+        pickle.dump(hist.history, f)
+        f.close()
+        return
+
+
+train = ImageDataGenerator(rescale = 1/255)
+validation = ImageDataGenerator(rescale = 1/255)            
+train_dataset = train.flow_from_directory('G:/My Projects/computer-vision projects/basedata/train/', target_size = (64,64), batch_size = 3, class_mode = 'binary')
+validation_dataset = validation.flow_from_directory('G:/My Projects/computer-vision projects/basedata/validation/', target_size = (64,64), batch_size = 3, class_mode = 'binary')
+
+train_dataset.class_indices
+
+datset = Dataset_Generation(train_dataset,validation_dataset,None,None,3) # 3 refers to the last layer computing softmax probabilities for 3 classes
+Architecture = Network_layers(train_dataset,validation_dataset,None,None,3)
+Architecture.layers()
+Architecture.compile_fit_model()
+
+
+
