1f430699a1d286147b361b4f5001e911143e8685
Ashwin M Jois <63425115+AshwinJois@users.noreply.github.com>
2021-04-22 15:33:38 +0530

    Update Task_2.py

--- ai_application/Task_2.py
+++ ai_application/Task_2.py
@@ -7,31 +7,17 @@ Created on Wed Apr 21 14:53:29 2021
 
 import tensorflow as tf
 import sys
-sys.path.append('G:/ICS_Sylabus/Object_Oriented_Programming/repo_ai_application')
+sys.path.append('enter the path of the file Task_1')
 from Task_1 import Dataset_Generation
 from Task_1 import Network_layers
 
 mnist = tf.keras.datasets.mnist # 28x28 images of handwritten digits 0-9
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-
-dataset_mnist = Dataset_Generation(x_train,y_train, x_test, y_test,10)
-
+dataset_mnist = Dataset_Generation(x_train,y_train, x_test, y_test,10) # 10 refers to the last layer computing softmax probabilities for 10 classes
 Network_mnist = Network_layers(x_train,y_train, x_test, y_test,10)
 
 Network_mnist.layers()
 Network_mnist.compile_fit_model()
 
-"""
-import numpy as np
-import matplotlib.pyplot as plt
 
-model = tf.keras.models.load_model('C:/Users/ashwi/OneDrive/Desktop/Travel March 2021/models', 
-                                          compile=True)
-
-prediction = model.predict([x_test])
-print(prediction)
-
-print(np.argmax(prediction[1000]))
-plt.imshow(x_test[1000])
-"""
